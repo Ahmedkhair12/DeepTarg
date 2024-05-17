a9e1bb145ad3af214c615cc40830884ee52b78ad
Ahmedkhair12 <96844293+Ahmedkhair12@users.noreply.github.com>
2024-05-17 16:49:11 +0200

    Update bootstrapping.sh

--- bootstrapping.sh
+++ bootstrapping.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Loop 5 times
+# iteration
 for ((i=1; i<=100; i++)); do
     echo "Iteration $i"
 
     # Execute the first Python file
-    python loop_DeepT.py
+    python DeepTarg_gnn.py
 
     # Execute the second Python file
     python deployment.py
