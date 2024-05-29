8a923f6c72302f87668544f23f0521b4639c9718
Ahmedkhair12 <96844293+Ahmedkhair12@users.noreply.github.com>
2024-05-30 01:04:50 +0200

    Update bootstrapping.sh

--- bootstrapping.sh
+++ bootstrapping.sh
@@ -5,7 +5,7 @@ for ((i=1; i<=100; i++)); do
     echo "Iteration $i"
 
     # Execute the first Python file
-    python DeepTarg_gnn.py
+    python training_and_evaluation.py
 
     # Execute the second Python file
     python model_deployment.py
