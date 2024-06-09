41a608393824f70d8a16e85cf77968fbdfa39a88
Ahmedkhair12 <96844293+Ahmedkhair12@users.noreply.github.com>
2024-06-09 17:58:30 +0200

    Update bootstrapping.sh

--- bootstrapping.sh
+++ bootstrapping.sh
@@ -4,10 +4,10 @@
 for ((i=1; i<=100; i++)); do
     echo "Iteration $i"
 
-    # Execute the first Python file
+    # training
     python training_and_evaluation.py
 
-    # Execute the second Python file
+    # deployment
     python model_deployment.py
 
     echo "Iteration $i completed"
